37c2402bf2e6ef74dc721ef991aa825178f41cb3
Doyita-Mitra <37648370+Doyita-Mitra@users.noreply.github.com>
2019-08-28 13:48:11 -0500

    Update US_map.py

--- US_map.py
+++ US_map.py
@@ -12,6 +12,7 @@ lon = list(data["LON"])
 #Load the elevation data from the Volcanoes.txt file into a list
 elevation = list(data["ELEV"])
 
+#Function for color generator as per elevation
 def color_generator(elevation):
     if elevation < 1000:
         return 'green'
@@ -22,23 +23,22 @@ def color_generator(elevation):
 
 map = folium.Map(location = [38.58, -99.09], zoom_start = 3.5, tiles = "Stamen Terrain")
 
-#Adding elements to the Map
+#Creating a feature group instead of all the 62 elements in the volcanoes file
 fgv = folium.FeatureGroup(name="Volcanoes")
 
 #Looping through the latitudes and longitudes by zipping the coordinates
 for lt, ln, el in zip(lat, lon, elevation):
-    fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 4, popup = str(el) + " m", \
+    fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 4, popup = str(el) + " m",
     fill_color = color_generator(el), color = 'grey', fill_opacity = 1))
 
-
+#Creating a feature group instead of all the elements in the World.json file
 fgp = folium.FeatureGroup(name="Population")
 
 fgp.add_child(folium.GeoJson(data = data_json), style_function = lambda x: {'fillColor': 'yellow' if x['properties']
-['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else
-'red' })
+['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' })
 
 map.add_child(fgv)
 map.add_child(fgp)
 map.add_child(folium.LayerControl())
 
-map.save("Map2.html")
+map.save("Map1.html")
